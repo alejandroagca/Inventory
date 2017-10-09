5454e634b76723808943c400d3fa1e20a0b5db97
alejandroagca <alejandroagca1995@gmail.com>
2017-10-09 10:40:25 +0200

    Primer commit

---
+++ app/src/main/java/com/example/Inventory/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.Inventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * LoginLinearLayout
+     * @author Alejandro Aguilar
+     * @version 1.0
+     * @see android.app.Activity
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
